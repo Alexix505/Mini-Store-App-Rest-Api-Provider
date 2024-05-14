f79b25416d3aaabb46ad101ff66706b8b90c33b5
Alexander Ikenna Orisa <115145901+Alexix505@users.noreply.github.com>
2024-05-14 15:03:09 +0100

    images added and theme set

---
+++ lib/const/global_colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+const Color lightScaffoldColor = Color(0xfff8f8f8);
+
+const lightCardColor = Color(0xffffffff);
+
+const lightBackgroudColor = Color(0xffd0e8f2);
+
+const lightIconsColor = Color.fromARGB(255, 175, 0, 120);
+
+const lightTextColor = Color(0xff324558);
