240572c4c5451fccd5684a6df2a29b2d7045b108
Alexander Ikenna Orisa <115145901+Alexix505@users.noreply.github.com>
2024-05-16 14:48:45 +0100

    updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -101,7 +101,10 @@ class _HomeScreenState extends State<HomeScreen> {
                   },
                 ),
               ),
-              FeedWidget()
+              SizedBox(
+                child: FeedWidget(),
+                width: 160,
+              ),
             ],
           ),
         ),
--- lib/widgets/feeds_widgets.dart
+++ lib/widgets/feeds_widgets.dart
@@ -10,39 +10,82 @@ class FeedWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return Material(
-      color: Theme.of(context).cardColor,
-      borderRadius: BorderRadius.circular(8),
-      child: InkWell(
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: Material(
+        color: Theme.of(context).cardColor,
         borderRadius: BorderRadius.circular(8),
-        onTap: () {},
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                RichText(
-                  text: TextSpan(
-                    text: '\$',
-                    style: TextStyle(
-                      color: Color.fromRGBO(33, 150, 243, 1),
-                      fontWeight: FontWeight.w600,
-                    ),
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: '168.00',
-                        style: TextStyle(
-                          color: lightTextColor,
-                          fontWeight: FontWeight.w600,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(8),
+          onTap: () {},
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 5, right: 5, top: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Flexible(
+                      child: RichText(
+                        text: const TextSpan(
+                          text: '\$',
+                          style: TextStyle(
+                            color: Color.fromRGBO(33, 150, 243, 1),
+                          ),
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: '168.00',
+                              style: TextStyle(
+                                color: lightTextColor,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ],
+                    ),
+                    const Icon(IconlyBold.heart),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: FancyShimmerImage(
+                  height: size.height * 0.2,
+                  width: double.infinity,
+                  errorWidget: const Icon(
+                    IconlyBold.danger,
+                    color: Colors.red,
+                    size: 28,
+                  ),
+                  imageUrl: "https://i.ibb.co/vwB46Yq/shoes.png",
+                  boxFit: BoxFit.fill,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Title',
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w700,
                   ),
                 ),
-                Icon(IconlyBold.heart),
-              ],
-            )
-          ],
+              ),
+              SizedBox(
+                height: size.height * 0.01,
+              ),
+            ],
+          ),
         ),
       ),
     );
