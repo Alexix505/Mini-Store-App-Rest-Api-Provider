89ad96c9b5919956bc960f698bed0cc6b70f9c70
Alexander Ikenna Orisa <115145901+Alexix505@users.noreply.github.com>
2024-05-16 08:18:51 +0100

    Swiper imported

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:mini_store_app_with_restapi_and_provider/const/global_colors.dart';
 import '../widgets/appbar_icons.dart';
 import '../widgets/sale_widget.dart';
+import 'package:card_swiper/card_swiper.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -14,6 +15,8 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return GestureDetector(
       onTap: () {
         FocusScope.of(context).unfocus();
@@ -34,35 +37,46 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ],
         ),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 15),
-              child: TextField(
-                decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    borderSide: const BorderSide(color: Colors.black12),
-                  ),
-                  focusedBorder: OutlineInputBorder(
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10.0),
-                      borderSide: BorderSide(
-                        width: 1,
-                        color: Theme.of(context).colorScheme.secondary,
-                      )),
-                  fillColor: Theme.of(context).cardColor,
-                  filled: true,
-                  hintText: 'Search',
-                  border: InputBorder.none,
-                  suffixIcon: const Icon(
-                    IconlyLight.search,
-                    color: lightIconsColor,
+                      borderSide: const BorderSide(color: Colors.black12),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Theme.of(context).colorScheme.secondary,
+                        )),
+                    fillColor: Theme.of(context).cardColor,
+                    filled: true,
+                    hintText: 'Search',
+                    border: InputBorder.none,
+                    suffixIcon: const Icon(
+                      IconlyLight.search,
+                      color: lightIconsColor,
+                    ),
                   ),
                 ),
               ),
-            ),
-            SalesWidget(),
-          ],
+              SizedBox(
+                height: size.height * 0.25,
+                child: Swiper(
+                  itemCount: 3,
+                  itemBuilder: (BuildContext ctx, index) {
+                    return const SalesWidget();
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
