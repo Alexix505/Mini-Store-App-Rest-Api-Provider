c5f83e67de23c566b26b97b0100023d345555d45
Alexander Ikenna Orisa <115145901+Alexix505@users.noreply.github.com>
2024-05-19 16:37:31 +0100

    api handler completed

--- lib/services/api_handler.dart
+++ lib/services/api_handler.dart
@@ -1,11 +1,14 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:mini_store_app_with_restapi_and_provider/const/api_consts.dart';
 
 class APIHandler {
   static Future<void> getAllProducts() async {
+    var uri = Uri.https(BASE_URL, 'api/v1/products');
+
     var response = await http.get(
-      Uri.parse('https://api.escuelajs.co/api/v1/products'),
+      uri,
     );
 
     print('response ${jsonDecode(response.body)}');
